218d9ce209de777ece6d1f6d5cf3a4fa91ff072d
LeGend <98607285+LeGend077@users.noreply.github.com>
2023-08-26 21:33:47 +0530

    Update 4

--- packs/BP/scripts/Main.js
+++ packs/BP/scripts/Main.js
@@ -1,1 +1,8 @@
 import { world, system } from "@minecraft/server";
+
+world.afterEvents.chatSend.subscribe(ev => {
+	let message = ev.message
+	if (message == 'show_objectives') {
+		ev.sender.runCommandAsync(`title @s title legend.objectives.§lObjectives§r:\n- Reach the Crypt`)
+	}
+})
